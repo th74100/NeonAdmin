67b3dbfeeab95a95deeea97570e2664948f178d8
th74100 <100682190+th74100@users.noreply.github.com>
2025-07-19 09:00:13 +0800

    update: build
    update: build

--- vite.config.js
+++ vite.config.js
@@ -63,6 +63,73 @@ export default defineConfig(({ mode }) => {
                 ],
             },
         },
+        build: {
+            minify: 'terser',
+            outDir: 'dist',
+            sourcemap: false,
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    pure_funcs: ['console.log'],
+                },
+            },
+            chunkSizeWarningLimit: 800,
+            reportCompressedSize: false,
+            assetsInlineLimit: 5 * 1024,
+            rollupOptions: {
+                output: {
+                    hashCharacters: 'hex',
+                    entryFileNames: 'assets/js/app.[hash].js',
+                    chunkFileNames: 'assets/js/[hash].js',
+                    assetFileNames(assetInfo) {
+                        // css
+                        if (Array.isArray(assetInfo.names)) {
+                            const name = assetInfo.names[0]
+                            if (name.endsWith('.css')) {
+                                return 'assets/css/[hash].css'
+                            }
+
+                            // 图片
+                            const imgExts = [
+                                '.png',
+                                '.jpg',
+                                '.jpeg',
+                                '.gif',
+                                '.ico',
+                                '.svg',
+                                '.webp',
+                            ]
+                            if (imgExts.some((ext) => name.endsWith(ext))) {
+                                return 'assets/images/[hash].[ext]'
+                            }
+
+                            // 字体
+                            const fontExts = [
+                                '.woff',
+                                '.woff2',
+                                '.eot',
+                                '.ttf',
+                                '.otf',
+                            ]
+                            if (fontExts.some((ext) => name.endsWith(ext))) {
+                                return 'assets/fonts/[hash].[ext]'
+                            }
+                        }
+                        return 'assets/[hash].[ext]'
+                    },
+                    experimentalMinChunkSize: 50 * 1024, // 60kb
+                    manualChunks: {
+                        vendor: ['vue', 'vue-router'],
+                        pinia: ['pinia', 'pinia-plugin-persistedstate'],
+                        element: ['element-plus'],
+                        libs: ['@element-plus/icons-vue'],
+                    },
+                },
+            },
+        },
+        optimizeDeps: {
+            include: ['vue', 'vue-router', 'element-plus'],
+        },
         server: {
             port: env.VITE_APP_PORT || 3000,
             host: '0.0.0.0', // 允许本机IP访问 0.0.0.0
